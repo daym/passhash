53a47a3e3b26bf1309eb413962e9e1128c319f11
Danny Milosavljevic <dannym@scratchpost.org>
2023-02-12 02:38:56 +0100

    Initial import.

---
+++ content.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    /*console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");*/
+    if (request.password) {
+      document.querySelectorAll("input[type='password']")
+        .forEach((input) => {
+          input.value = request.password;
+      });
+      sendResponse({ok: "ok"});
+    }
+  }
+);
